f81892ec324b1e0e72fcda62729187c14aa87d73
Vaibhav4556 <burdevaibhav786@gmail.com>
2023-06-20 13:44:23 +0530

    commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+ 
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
+        <div className="loveText">Made By VAIBHAV </div>
+        <div className="socialIcons">
+          <a
+            className="link"
+            href="https://github.com/Vaibhav4556"
+            
+          >
+            <span className="icon">
+              <FaGithub />
+            </span>
+          </a>
+         
+         
+          <a
+            className="link"
+            href="https://www.linkedin.com/in/vaibhavburde/"
+            
+          >
+            <span className="icon">
+              <FaLinkedin />
+            </span>
+          </a>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
